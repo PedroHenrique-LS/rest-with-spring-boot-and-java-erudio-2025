7f403a395e218d9a01db7af87631cc7ac35497e8
Pedro Henrique <ph.laurindo2020@gmail.com>
2025-04-06 10:13:10 -0300

    secão 11: Adicionando Suporte à migrations com Flyway)

---
+++ rest-with-spring-boot-and-java-erudio-2025/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `larst_name`) VALUES 
+(3,'Rua','Pedro','Male','Laurindo'),
+(5,'Rua','Pedro','Male','Laurindo'),
+(6,'Minha casa','Jorge','Male','Laurindo'),
+(8,'Minha casa','Jorge','Male','Laurindo'),
+(9,'Minha casa','Jorge Teste V2','Male','Laurindo'),
+(10,'Minha casa','Jorge Teste V1','Male','Laurindo');
+
+
